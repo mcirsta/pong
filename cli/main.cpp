451ec9d3b4d7babf130b17e0f647444e4c9dbd60
Marius Cirsta <mforce2@gmail.com>
2015-10-06 16:39:45 +0300

    * begin adding boost command line parsing support

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 
+#include <boost/program_options.hpp>
+namespace boost_po = boost::program_options;
+
 int main (int argc, char *argv[]) 
 {
-    if(argc<2) {
-	std::cout<<"incorrect number of arguments"<<std::endl;
+    boost_po::options_description mainopts("Main options");
+    mainopts.add_options()
+            ("help,h",    "produce a help message")
+            ("version,V", "output the version number")
+            ("add,A",     "add fpm file")
+            ("remove,R",  "remove package")
+            ("upgrade,U", "upgrade fpm file")
+            ("freshen,F", "feshen fpm file")
+            ("query,Q",   "query package")
+            ("sync,S",    "sync")
+            ("ps,P",      "ps")
+            ("vercmp,Y",  "compare versions");
+            ;
+
+    boost_po::options_description add("Add options");
+    boost_po::options_description remove("Remove options");
+    boost_po::options_description upgrade("Upgrade options");
+    boost_po::options_description freshen("Freshen options");
+    boost_po::options_description query("Query options");
+    boost_po::options_description sync("Sync options");
+    boost_po::options_description ps("ps options");
+    boost_po::options_description vercmp("Vercmp options");
+
+    boost_po::variables_map mainvm;
+    boost_po::store(boost_po::parse_command_line(argc, argv, mainopts), mainvm);
+
+    if(mainvm.count("help") || argc<=1) {
+        std::cout<<mainopts;
     }
 }
