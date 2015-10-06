b5dc457a3140b8d06f09be44999094275dc2484a
Marius Cirsta <mcirsta@frugalware.org>
2015-10-06 19:06:10 +0300

    * print version

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,7 +1,22 @@
+#define PACKAGE_VERSION 0.1
+#define PM_VERSION      0.1
+
 #include <iostream>
 
 #include <boost/program_options.hpp>
 namespace boost_po = boost::program_options;
+
+void printCliVersion()
+{
+    std::cout<<std::endl;
+    std::cout<<"              |    Pong v"<<PACKAGE_VERSION<<" - libpong v"<< PM_VERSION<<std::endl;
+    std::cout<<"                   Copyright (C) 2015 Marius Cirsta\n";
+    std::cout<<"     o             See /usr/share/doc/pong-"<<PACKAGE_VERSION<<"/AUTHORS for more info."<<std::endl;
+    std::cout<<"|                  "<<std::endl;
+    std::cout<<"                   This program may be freely redistributed under"<<std::endl;
+    std::cout<<"                   the terms of the GNU General Public License, version 3"<<std::endl;
+    std::cout<<std::endl;
+}
 
 int main (int argc, char *argv[]) 
 {
@@ -16,10 +31,15 @@ int main (int argc, char *argv[])
             ("query,Q",   "query package")
             ("sync,S",    "sync")
             ("ps,P",      "ps")
-            ("vercmp,Y",  "compare versions");
+            ("vercmp,Y",  "compare versions")
             ;
 
     boost_po::options_description add("Add options");
+    add.add_options()
+            ("nodeps,d",   "skip dependency checks")
+            ("force,f",    "force install, overwrite conflicting files")
+            ("noarch",     "install the package, even if it is for an other arch")
+            ;
     boost_po::options_description remove("Remove options");
     boost_po::options_description upgrade("Upgrade options");
     boost_po::options_description freshen("Freshen options");
@@ -33,5 +53,43 @@ int main (int argc, char *argv[])
 
     if(mainvm.count("help") || argc<=1) {
         std::cout<<mainopts;
+        EXIT_SUCCESS;
     }
+
+    if(mainvm.count("version")) {
+        printCliVersion();
+        EXIT_SUCCESS;
+    }
+
+    if(mainvm.count("add") || argc<=1) {
+        std::cout<<mainopts;
+    }
+
+    if(mainvm.count("remove") || argc<=1) {
+        std::cout<<mainopts;
+    }
+
+    if(mainvm.count("upgrade") || argc<=1) {
+        std::cout<<mainopts;
+    }
+
+    if(mainvm.count("freshen") || argc<=1) {
+        std::cout<<mainopts;
+    }
+
+    if(mainvm.count("query") || argc<=1) {
+        std::cout<<mainopts;
+    }
+    if(mainvm.count("sync") || argc<=1) {
+        std::cout<<mainopts;
+    }
+
+    if(mainvm.count("ps") || argc<=1) {
+        std::cout<<mainopts;
+    }
+
+    if(mainvm.count("vercmp") || argc<=1) {
+        std::cout<<mainopts;
+    }
+
 }
