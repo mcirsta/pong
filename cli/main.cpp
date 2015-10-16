6b96e8eef0c2625173e1dd4703b0f2e8015a20ab
Marius Cirsta <mcirsta@frugalware.org>
2015-10-16 18:52:45 +0300

    * continue cmd parser work

--- cli/main.cpp
+++ cli/main.cpp
@@ -6,6 +6,30 @@
 #include <boost/program_options.hpp>
 namespace boost_po = boost::program_options;
 
+struct mainOpts {
+    char shortName;
+    std::string longName;
+    std::string initialVal;
+    bool mainOpt;
+    std::string description;
+};
+
+struct secondaryOpts {
+    char shortName;
+    std::string longName;
+    std::string initialVal;
+    bool mainOpt;
+    std::string description;
+};
+
+std::vector<cmdLineStruct> cmdOpts = { {'h', "help",     "produce a help message"    },
+
+
+
+
+    {'f', "force", "0", false, "force install, overwrite conflicting"}
+                                     };
+
 void printCliVersion()
 {
     std::cout<<std::endl;
@@ -32,58 +56,51 @@ void printCliVersion()
 
 int main (int argc, char *argv[]) 
 {
-    boost_po::options_description mainopts("Main options");
-    mainopts.add_options()
-            ("help,h",    "produce a help message")
-            ("version,V", "output the version number")
-            ("add,A",     "add fpm file")
-            ("remove,R",  "remove package")
-            ("upgrade,U", "upgrade fpm file")
-            ("freshen,F", "feshen fpm file")
-            ("query,Q",   "query package")
-            ("sync,S",    "sync")
-            ("ps,P",      "ps")
-            ("vercmp,Y",  "compare versions")
+    boost_po::options_description allOpts("All options");
+    mainOpts.add_options()
+            ("help,h",          "produce a help message")
+            ("version,V",       "output the version number")
+            ("add,A",           "add fpm file")
+            ("remove,R",        "remove package")
+            ("upgrade,U",       "upgrade fpm file")
+            ("freshen,F",       "feshen fpm file")
+            ("query,Q",         "query package")
+            ("sync,S",          "sync")
+            ("ps,P",            "ps")
+            ("vercmp,Y",        "compare versions")
+            ("input", boost_po::value< std::vector<std::string> >(), "input file")
+            ("nodeps,d",        "skip dependency checks")
+            ("noarch",          "install the package, even if it is for an other arch")
+            ("cascade,c",       "remove packages and all packages that depend on them")
+            ("dbonly,k",        "only remove database entry, do not remove files")
+            ("nosave,n",        "remove configuration files as well")
+            ("recursive,s",     "remove dependencies also (that won't break packages)")
+            ("force,f",         "force install, overwrite conflicting")
             ;
 
-    boost_po::options_description addOpts("Add options");
-    addOpts.add_options()
-            ("nodeps,d",   "skip dependency checks")
-            ("force,f",    "force install, overwrite conflicting files")
-            ("noarch",     "install the package, even if it is for an other arch")
-            ;
-    boost_po::options_description remove("Remove options");
-    remove.add_options()
-    ("cascade,c",       "remove packages and all packages that depend on them")
-    ("nodeps,d",        "skip dependency checks")
-    ("dbonly,k",        "only remove database entry, do not remove files")
-    ("nosave,n",        "remove configuration files as well")
-    ("recursive,s",     "remove dependencies also (that won't break packages)")
-    ;
-    boost_po::options_description upgrade("Upgrade options");
-    boost_po::options_description freshen("Freshen options");
-    boost_po::options_description query("Query options");
-    boost_po::options_description sync("Sync options");
-    boost_po::options_description ps("ps options");
-    boost_po::options_description vercmp("Vercmp options");
+    allOpts.add(mainOpts).add(addOpts).add(removeOpts).add(upgradeOpts).add(queryOpts).add(syncOpts).
+            add(psOpts).add(vercmpOpts);
 
     boost_po::variables_map mainvm;
 
-//    try {
+    //    try {
 
-//    boost_po::store(boost_po::parse_command_line(argc, argv, mainopts), mainvm);
+    //    boost_po::store(boost_po::parse_command_line(argc, argv, mainopts), mainvm);
 
-//    }
-//    catch(std::exception& e) {
-//        std::cout << e.what() << "\n";
-//    }
+    //    }
+    //    catch(std::exception& e) {
+    //        std::cout << e.what() << "\n";
+    //    }
 
-    boost_po::parsed_options parsedMain = boost_po::command_line_parser(argc, argv).options(mainopts).allow_unregistered().run();
+    boost_po::positional_options_description p;
+    p.add("input", -1);
+
+    boost_po::parsed_options parsedMain = boost_po::command_line_parser(argc, argv).options(allOpts).positional(p).run();
 
     boost_po::store(parsedMain, mainvm);
 
     if(mainvm.count("help") || argc<=1) {
-        std::cout<<mainopts;
+        std::cout<<mainOpts;
         EXIT_SUCCESS;
     }
 
@@ -92,43 +109,115 @@ int main (int argc, char *argv[])
         EXIT_SUCCESS;
     }
 
-    std::vector<std::string> remainingOpts = boost_po::collect_unrecognized(parsedMain.options, boost_po::include_positional);
-
     if(mainvm.count("add")) {
-        std::cout<<"add chosen and rest to parse is "<<remainingOpts[0]<<std::endl;
-        boost_po::variables_map addvm;
-        addOpts.add(mainopts);
-        parsedMain = boost_po::command_line_parser(remainingOpts).options(addOpts).allow_unregistered().run();
-        boost_po::store(parsedMain, addvm);
-        if(addvm.count("nodeps")) {
-            std::cout<<"no deps chosen"<<std::endl;
+        mainvm.erase("add");
+        std::cout<<"add chosen"<<std::endl;
+        for(const auto &i:mainvm) {
+            if(i.first == "nodeps") {
+                std::cout<<"nodeps chosen"<<std::endl;
+            }
+            else if(i.first == "force") {
+                std::cout<<"force chosen"<<std::endl;
+            }
+            else if(i.first == "noarch") {
+                std::cout<<"dbonly chosen"<<std::endl;
+            }
+            else if(i.first == "input") {
+                std::vector<std::string> inputNames = i.second.as< std::vector<std::string> >();
+                std::cout<<"input: ";
+                for(const auto& iname:inputNames) {
+                     std::cout<<iname<<" ";
+                }
+                std::cout<<std::endl;
+            }
+            else {
+                std::cout<<"invalid add option "<<i.first<<std::endl;
+                std::cout<<addOpts<<std::endl;
+            }
         }
     }
 
     if(mainvm.count("remove")) {
-        std::cout<<mainopts;
+        mainvm.erase("remove");
+        std::cout<<"remove chosen"<<std::endl;
+        for(const auto &i:mainvm) {
+            if(i.first == "cascade") {
+                std::cout<<"cascade chosen"<<std::endl;
+            }
+            else if(i.first == "nodeps") {
+                std::cout<<"nodeps chosen"<<std::endl;
+            }
+            else if(i.first == "dbonly") {
+                std::cout<<"dbonly chosen"<<std::endl;
+            }
+            else if(i.first == "nosave") {
+                std::cout<<"dbonly chosen"<<std::endl;
+            }
+            else if(i.first == "recursive") {
+                std::cout<<"dbonly chosen"<<std::endl;
+            }
+            else if(i.first == "input") {
+                std::vector<std::string> inputNames = i.second.as< std::vector<std::string> >();
+                std::cout<<"input: ";
+                for(const auto& iname:inputNames) {
+                     std::cout<<iname<<" ";
+                }
+                std::cout<<std::endl;
+            }
+            else {
+                std::cout<<"invalid remove option "<<i.first<<std::endl;
+                std::cout<<removeOpts<<std::endl;
+            }
+        }
     }
 
-    if(mainvm.count("upgrade")) {
-        std::cout<<mainopts;
+    if(mainvm.count("upgrade") && mainvm.count("freshen")) {
+        //freshen and upgrade are almost the same, freshen is just a special upgrade case
+        std::string upname = "";
+        if(mainvm.count("freshen")) {
+            upname = "freshen";
+            mainvm.erase("freshen");
+            std::cout<<"freshen chosen"<<std::endl;
+        }
+        else {
+            upname = "upgrade";
+            mainvm.erase("upgrade");
+            std::cout<<"upgrade chosen"<<std::endl;
+        }
+        for(const auto &i:mainvm) {
+            if(i.first == "nodeps") {
+                std::cout<<"nodeps chosen"<<std::endl;
+            }
+            else if(i.first == "force") {
+                std::cout<<"force chosen"<<std::endl;
+            }
+            else if(i.first == "input") {
+                std::vector<std::string> inputNames = i.second.as< std::vector<std::string> >();
+                std::cout<<"input: ";
+                for(const auto& iname:inputNames) {
+                     std::cout<<iname<<" ";
+                }
+                std::cout<<std::endl;
+            }
+            else {
+                std::cout<<"invalid "<<upname<<" option "<<i.first<<std::endl;
+                std::cout<<removeOpts<<std::endl;
+            }
     }
-
-    if(mainvm.count("freshen")) {
-        std::cout<<mainopts;
     }
 
     if(mainvm.count("query")) {
-        std::cout<<mainopts;
+        std::cout<<mainOpts;
     }
     if(mainvm.count("sync")) {
-        std::cout<<mainopts;
+        std::cout<<mainOpts;
     }
 
     if(mainvm.count("ps")) {
-        std::cout<<mainopts;
+        std::cout<<mainOpts;
     }
 
     if(mainvm.count("vercmp")) {
-        std::cout<<mainopts;
+        std::cout<<mainOpts;
     }
 }
