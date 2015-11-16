78136e623441bd11fdb7981318b487f9ee7e6b45
Marius Cirsta <mcirsta@frugalware.org>
2015-11-16 12:16:44 +0200

    * begin adding sqlite support

--- lib/database.cpp
+++ lib/database.cpp
@@ -1,7 +1,8 @@
 #include "database.hpp"
 #include "utils.hpp"
 
-#include "stdio.h"
+#include <cstdio>
+#include <sqlite3.h>
 
 bool extractOldDB(FILE *dbFile) {
     extractXZ(dbFile, "/tmp/pongTemp/");
@@ -21,7 +22,19 @@ bool openLegacyDB(std::string dbpath) {
 }
 
 bool createNewDB(std::string dbpath) {
-
+    std::string dbName = dbpath + "/" + "frugalware-current.pdb";
+    sqlite3 *dbHandle;
+    int r = sqlite3_open_v2(dbName.c_str(), &dbHandle, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_FULLMUTEX
+                            | SQLITE_OPEN_PRIVATECACHE, nullptr);
+    if(r != SQLITE_OK) {
+        std::cout<<"error while opening Sqlite database: "<<r;
+        return false;
+    }
+    sqlite3_stmt *sqlStmt;
+    const char *createStr = "CREATE TABLE Packages ( \
+                            pkgId int,"
+                                      ")";
+    r = sqlite3_prepare_v2(dbHandle, createStr, -1, &sqlStmt, nullptr);
 }
 
 bool importData(std::string dbpath) {
@@ -31,6 +44,6 @@ bool importData(std::string dbpath) {
 bool initDB(std::string dbpath) {
     std::cout<<"database path: "<<dbpath<<std::endl;
     openLegacyDB(dbpath);
-    createNewDB("");
+    createNewDB("/tmp/pongTemp");
     importData("");
 }
