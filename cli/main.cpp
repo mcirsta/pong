ea33db29a25d575ac8f53846c69c54a6c6c496f2
Bogdan Voiculescu <bogdan.voiculescu89@yahoo.com>
2016-10-17 00:06:43 +0300

    main should have one return

--- cli/main.cpp
+++ cli/main.cpp
@@ -12,6 +12,7 @@ int main (int argc, char *argv[])
     boost::program_options::variables_map secondaryOpts;
     P_OP mainOption = cliConf.getConfig(secondaryOpts);
     bool libInitDone = false;
+    bool opSucceded = true;
 
     if(isLibInitRequired(mainOption)) {
         //we need to init the database for these options
@@ -19,50 +20,42 @@ int main (int argc, char *argv[])
                 secondaryOpts["dbpath"].as<std::string>());
         if(!libInitDone) {
             std::cout<<"could not initialize pong library"<<std::endl;
-            return 1;
+            opSucceded = false;
+        }
+        else {
+            std::string libRet = "";
+            switch(mainOption) {
+            case P_OP::OP_ADD :
+                opSucceded = addPackages();
+                break;
+            case P_OP::OP_DATABASE :
+                opSucceded = databaseDispatcher(secondaryOpts, libRet);
+                std::cout<<"pong says: "<<libRet<<std::endl;
+                break;
+            case P_OP::OP_UPGRADE:
+            case P_OP::OP_FRESHEN:
+            case P_OP::OP_REMOVE:
+            case P_OP::OP_QUERY:
+            case P_OP::OP_SYNC:
+            case P_OP::OP_PS:
+            case P_OP::OP_VERCMP:
+                std::cout<<"option not implemented"<<std::endl;
+            default:
+                opSucceded = true;
+                break;
+            }
+
+            if(!pongLibClose()) {
+                std::cout<<"could not properly close pong library"<<std::endl;
+                opSucceded = false;
+            }
         }
     }
-
-    std::string libRet = "";
-    bool opSucceded = true;
-    switch(mainOption) {
-    case P_OP::OP_UNKNOWN:
-        //do nothing, exit successfully
-        opSucceded = true;
-        break;
-    case P_OP::OP_VERSION:
-        //print version and exit successfully
-        printCliVersion();
-        opSucceded = true;
-        break;
-    case P_OP::OP_HELP:
-        //help message already printed, exit successfully
-        opSucceded = true;
-        break;
-    case P_OP::OP_ADD :
-        opSucceded = addPackages();
-        break;
-    case P_OP::OP_DATABASE :
-        opSucceded = databaseDispatcher(secondaryOpts, libRet);
-        std::cout<<"pong says: "<<libRet<<std::endl;
-        break;
-    case P_OP::OP_UPGRADE:
-    case P_OP::OP_FRESHEN:
-    case P_OP::OP_REMOVE:
-    case P_OP::OP_QUERY:
-    case P_OP::OP_SYNC:
-    case P_OP::OP_PS:
-    case P_OP::OP_VERCMP:
-        std::cout<<"option not implemented"<<std::endl;
-        opSucceded = true;
-        break;
-    }
-
-    if(libInitDone) {
-        if(!pongLibClose()) {
-            std::cout<<"could not properly close pong library"<<std::endl;
-            return 1;
+    else {
+        if (mainOption == P_OP::OP_VERSION) {
+            printCliVersion();
         }
+        opSucceded = true;
     }
 
     int8_t programStatus = opSucceded ? 0 : 1;
