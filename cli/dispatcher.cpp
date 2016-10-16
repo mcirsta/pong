9206335709b25374befbdbcd7e490494a7ca4897
Bogdan Voiculescu <bogdan.voiculescu89@yahoo.com>
2016-10-16 16:10:14 +0300

    DB dispatcher should have one return

--- cli/dispatcher.cpp
+++ cli/dispatcher.cpp
@@ -4,19 +4,19 @@
 bool databaseDispatcher(const  boost::program_options::variables_map &secondaryOpts, std::string &retStr) {
     bool isQuerySuccessful = false;
     if(secondaryOpts.find("alldeps") != secondaryOpts.end()) {
-       return dbQuery(DBOpts::OP_ALL_DEPS, secondaryOpts["alldeps"].as<std::string>().c_str(), retStr);
+       isQuerySuccessful = dbQuery(DBOpts::OP_ALL_DEPS, secondaryOpts["alldeps"].as<std::string>().c_str(), retStr);
     }
     else
         if(secondaryOpts.find("rebuilddeps") != secondaryOpts.end()) {
-            return dbQuery(DBOpts::OP_REBUILD_DEPS, secondaryOpts["rebuilddeps"].as<std::string>().c_str(), retStr);
+            isQuerySuccessful = dbQuery(DBOpts::OP_REBUILD_DEPS, secondaryOpts["rebuilddeps"].as<std::string>().c_str(), retStr);
         }
         else
             if(secondaryOpts.find("revdeps") != secondaryOpts.end()) {
-                return dbQuery(DBOpts::OP_REV_DEPS, secondaryOpts["revdeps"].as<std::string>().c_str(), retStr);
+                isQuerySuccessful = dbQuery(DBOpts::OP_REV_DEPS, secondaryOpts["revdeps"].as<std::string>().c_str(), retStr);
             }
             else
                 if(secondaryOpts.find("directdeps") != secondaryOpts.end()) {
-                    return dbQuery(DBOpts::OP_DIRECT_DEPS, secondaryOpts["directdeps"].as<std::string>().c_str(), retStr);
+                    isQuerySuccessful = dbQuery(DBOpts::OP_DIRECT_DEPS, secondaryOpts["directdeps"].as<std::string>().c_str(), retStr);
                 }
     return isQuerySuccessful;
 }
