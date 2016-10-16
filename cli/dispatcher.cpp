7b6c4cb293fef29f6f76ad29b8bc4455b67e8c71
Bogdan Voiculescu <bogdan.voiculescu89@yahoo.com>
2016-10-16 16:15:11 +0300

    DB dispatcher improved code structure

--- cli/dispatcher.cpp
+++ cli/dispatcher.cpp
@@ -1,22 +1,21 @@
 #include "dispatcher.hpp"
 
 
-bool databaseDispatcher(const  boost::program_options::variables_map &secondaryOpts, std::string &retStr) {
+bool databaseDispatcher(const boost::program_options::variables_map &secondaryOpts, std::string &retStr) {
     bool isQuerySuccessful = false;
+
     if(secondaryOpts.find("alldeps") != secondaryOpts.end()) {
        isQuerySuccessful = dbQuery(DBOpts::OP_ALL_DEPS, secondaryOpts["alldeps"].as<std::string>().c_str(), retStr);
     }
-    else
-        if(secondaryOpts.find("rebuilddeps") != secondaryOpts.end()) {
-            isQuerySuccessful = dbQuery(DBOpts::OP_REBUILD_DEPS, secondaryOpts["rebuilddeps"].as<std::string>().c_str(), retStr);
-        }
-        else
-            if(secondaryOpts.find("revdeps") != secondaryOpts.end()) {
-                isQuerySuccessful = dbQuery(DBOpts::OP_REV_DEPS, secondaryOpts["revdeps"].as<std::string>().c_str(), retStr);
-            }
-            else
-                if(secondaryOpts.find("directdeps") != secondaryOpts.end()) {
-                    isQuerySuccessful = dbQuery(DBOpts::OP_DIRECT_DEPS, secondaryOpts["directdeps"].as<std::string>().c_str(), retStr);
-                }
+    else if(secondaryOpts.find("rebuilddeps") != secondaryOpts.end()) {
+       isQuerySuccessful = dbQuery(DBOpts::OP_REBUILD_DEPS, secondaryOpts["rebuilddeps"].as<std::string>().c_str(), retStr);
+    }
+    else if(secondaryOpts.find("revdeps") != secondaryOpts.end()) {
+       isQuerySuccessful = dbQuery(DBOpts::OP_REV_DEPS, secondaryOpts["revdeps"].as<std::string>().c_str(), retStr);
+    }
+    else if(secondaryOpts.find("directdeps") != secondaryOpts.end()) {
+       isQuerySuccessful = dbQuery(DBOpts::OP_DIRECT_DEPS, secondaryOpts["directdeps"].as<std::string>().c_str(), retStr);
+    }
+
     return isQuerySuccessful;
 }
